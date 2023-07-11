1dd5052981d7d7c79c38e7600270056ae21cc57f
khushbhadrecha02 <2101010101013@darshan.ac.in>
2023-07-11 23:39:17 +0530

    Add project files.

---
+++ Folio/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Folio.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Folio.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Portfolio()
+        {
+            return View();
+        }
+
+        public IActionResult Blog()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+
+
+
+
+
+    }
+}
